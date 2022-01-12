5c4cfd85319b4ac8bb3d08fdcfb63c531ac8298f
EmmanuelFrontend <emmanuel.engineer@dabsoluterobotics.com>
2022-01-12 17:48:26 +0100

    rce

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 const jsonServer = require("json-server");
 const jwt  = require("jsonwebtoken");
+const dotenv  = require("dotenv");
+const { OAuth2Client } = require('google-auth-library');
+
+dotenv.config();
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const server = jsonServer.create();
 const db = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
@@ -66,7 +71,7 @@ server.post("/api/auth/register", (req, res) => {
         )
     });
 
-    const access_token = createToken({email,password});
+    const access_token = createToken({email});
     res.status(200).json({token:access_token});
 });
 
@@ -95,11 +100,81 @@ server.post("/api/auth/login", (req, res) => {
     const id = userData.id;
     const userName = userData.fullname;
 
-    const access_token = createToken({id,name:userName,email,password});
-    res.status(200).json({
+    const access_token = createToken({id,name:userName,email});
+    res.status(201).json({
         name: userName,
         token:access_token
     });
+});
+
+server.post("/api/auth/google-login", async (req, res) => {
+    const {tokenId} = req.body;
+    const ticket = await client.verifyIdToken({
+        idToken: tokenId,
+        audience: process.env.GOOGLE_CLIENT_ID
+    });
+
+    const { name, email} = ticket.getPayload();
+    console.log(name,email,'name')
+
+    if(!email){
+        const status = 401;
+        const message = "Email is not valid";
+        res.status(status).json({status, message})
+        return;
+    }
+
+ 
+    const userData = db.users.find((user) => user.email === email);
+    if(userData){
+        const id = userData.id;
+        const usermail = userData.email;
+
+        const access_token = createToken({id,name,usermail});
+        res.status(201).json({
+            name: name,
+            token:access_token
+        });
+
+        return;
+        
+    }else{
+        fs.readFile("./db.json", (err,data)=>{
+            if(err){
+                const status = 401;
+                const message = err;
+                res.status(status).json({status, message})
+                return;
+            }
+            data = JSON.parse(data.toString());
+            let get_item = data.users[data.users.length -1].id
+            data.users.push({id: last_item_id + 1,fullname:name, email: email, password: null});
+            let writeData = fs.writeFile("./db.json", 
+                JSON.stringify(data),
+                (err, result) => {
+                    console.log(result,'re')
+                    if(err){
+                        const status = 401;
+                        const message = err;
+                        res.status(status).json({status, message})
+                        return;
+                    }
+                }
+            )
+        });
+        
+        console.log(writeData,'writeData')
+        const access_token = createToken({name,email});
+        res.status(201).json(
+            {
+                name: name,
+                token:access_token
+           });
+
+        return;
+    }
+
+    
 });
 
 server.post("/api/auth/forgotpassord", (req, res) => {
@@ -116,7 +191,47 @@ server.post("/api/auth/forgotpassord", (req, res) => {
     });
 });
 
+server.post("/api/auth/newpassword", (req, res) => {
+    console.log(req.body,'body')
+    const {email,password} = req.body;
+    if(!verifyEmail(email)){
+        const status = 401;
+        const message = "Email is not registered";
+        res.status(status).json({status, message})
+        return;
+    }
+
+    fs.readFile("./db.json", (err,data)=>{
+        if(err){
+            const status = 401;
+            const message = err;
+            res.status(status).json({status, message})
+            return;
+        }
+     
+        data = JSON.parse(data.toString());
+        let elementIndex = data.users.findIndex(user => user.email === email);
+        let newArray = [...data.users];
+        newArray[elementIndex] = {...newArray[elementIndex], password:password}
+        data.users = newArray;
+        let writeData = fs.writeFile("./db.json", 
+            JSON.stringify(data),
+            (err, result) => {
+                if(err){
+                    const status = 401;
+                    const message = err;
+                    res.status(status).json({status, message})
+                    return;
+                }
+            }
+        )
+     });
+    res.status(200).json({
+        success: true,
+    });
+});
+
 
 server.listen(process.env.PORT || 5000,()=> {
-    console.log("Runing Fake Json Server");
+    console.log("Running Fake Json Server");
 });
